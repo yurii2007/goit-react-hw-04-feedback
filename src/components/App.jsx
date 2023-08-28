2dc9f1961c0adafefefa1ff74e75f67227f4d3b6
yurii2007 <yurash862@gmail.com>
2023-08-28 15:28:37 +0300

    rework on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,51 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [feedbackOptions, setLeaveFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const leaveFeedback = key => {
+    setLeaveFeedback(prevState => ({
+      ...prevState,
+      [key]: prevState[key] + 1,
+    }));
   };
 
-  leaveFeedback = key => {
-    this.setState(prevState => ({ [key]: prevState[key] + 1 }));
-  };
-
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, el) => {
+  const countTotalFeedback = () => {
+    return Object.values(feedbackOptions).reduce((acc, el) => {
       return acc + el;
     }, 0);
   };
 
-  countPositiveFeedbackPercentage = () => parseInt((this.state.good * 100) / this.countTotalFeedback());
+  const countPositiveFeedbackPercentage = () =>
+    parseInt((feedbackOptions.good * 100) / countTotalFeedback());
 
-  render() {
-    return (
-      <>
-      <Section title='Please leave feedback'>
-      <FeedbackOptions
-          options={this.state}
-          feedbackCounter={this.leaveFeedback}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={feedbackOptions}
+          feedbackCounter={leaveFeedback}
         />
       </Section>
-      <Section title='Statistics'>
-        {this.countTotalFeedback() === 0 ? (
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
           <Notification message="There is no feedback" />
         ) : (
           <Statistics
-            {...this.state}
-            total={this.countTotalFeedback()}
-            percentage={this.countPositiveFeedbackPercentage()}
-          />)}
+            {...feedbackOptions}
+            total={countTotalFeedback()}
+            percentage={countPositiveFeedbackPercentage()}
+          />
+        )}
       </Section>
-      </>
-    );
-  }
-}
+    </>
+  );
+};
